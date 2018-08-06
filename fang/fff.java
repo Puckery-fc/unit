fe704c789528f39002d99253445f8b8e5492b1fe
花满楼 <691618966@qq.com>
2018-08-06 16:31:41 +0800

    fangcheng

---
+++ fang/fff.java
@@ -0,0 +1,18 @@
+package fang;
+
+public class fff {
+	public static void main(String[] args) {
+		for (int i = 2; i < 1000; i++) {
+			int sum = 0;
+			// 查找因数
+			for (int j = 1; j < i; j++) {
+				if (i % j == 0) {
+					sum += j;
+				}
+
+			}
+			if (sum == i)
+				System.out.println(i);
+		}
+	}
+}
